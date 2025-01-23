954d090ae45299b3b90c79a5d36911a431eb7738
junxiaozhang95 <junxiaozhang95@gmail.com>
2025-01-24 00:37:21 +0800

    update gddr6_checker

--- gddr6_example/gddr6_checker.sv
+++ gddr6_example/gddr6_checker.sv
@@ -1,19 +1,18 @@
-module gddr6_checker(
-    input           		CLK,
-    input  CLKB,
-input           		WCLK0, 
-input WCLK0B, 
-input WCLK1, 
-input WCLK1B,
-input           		CKEB, 
-input CABIB,
-input   [`ADDREA-1:0]   CA,
-inout   [`BIT_DQ]  		DQ,
-inout   [`nDBI-1:0]  	DBIB,
-inout   [`nEDC-1:0]  	EDC,
-
-input           		RESETB
+module gddr6_checker (
+    input        CLK,
+    input        CLKB,
+    input        WCLK0,
+    input        WCLK0B,
+    input        WCLK1,
+    input        WCLK1B,
+    input        CKEB,
+    input        CABIB,
+    input [10:0] CA,
+    input [15:0] DQ,
+    input [ 1:0] DBIB,
+    input [ 1:0] EDC,
+    input        RESETB
 
 );
-    
+
 endmodule
